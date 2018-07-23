c141c1be9179906f02309a80e6b83101514db9a2
Eduardo Maia <ehammo@cin.ufpe.br>
2018-07-23 18:07:40 -0300

    User Controller and routes

---
+++ controllers/userController.js
@@ -0,0 +1,97 @@
+'use strict'
+// why use strict? https://www.w3schools.com/js/js_strict.asp
+module.exports = function (tables) {
+  var userTable
+  
+  class UserController {
+    
+    constructor (tables) {
+      userTable = tables.User
+    }
+
+    createUser (User) {
+      return new Promise(function (resolve, reject) {
+        UserTable.create(
+          User
+        ).then((returnValue) => {
+          // returnValue can be null?
+          if (!returnValue) {
+            reject(new Error('error'))
+          }
+          resolve(returnValue)
+        }).catch((error) => {
+          reject(error)
+        })
+      })
+    }
+
+    readUser (email) {
+      return new Promise(function (resolve, reject) {
+        UserTable.findOne(
+          {
+            where: {
+              email: email
+            }
+          }
+        ).then((User) => {
+          if (!User) {
+            reject(new Error('No registered User with this Id'))
+          }
+          resolve(User)
+        }).catch((error) => {
+          reject(error)
+        })
+      })
+    }
+
+    readAllUsers () {
+      return new Promise(function (resolve, reject) {
+        UserTable.findAll().then((Users) => {
+          if (!Users) {
+            reject(new Error('No registered Users'))
+          }
+          resolve(Users)
+        })
+      })
+    }
+
+    updateUser (email, User) {
+      return new Promise(function (resolve, reject) {
+        UserTable.update(
+          User,
+          {
+            where: {
+              email: email
+            }
+          }).then((returnValue) => {
+          // returnValue can be null?
+          if (!returnValue) {
+            reject(new Error('erro'))
+          }
+          resolve('Updated ' + returnValue + ' User(s) sucessfully')
+        }).catch((error) => {
+          reject(error)
+        })
+      })
+    }
+
+    deleteUser (email) {
+      return new Promise(function (resolve, reject) {
+        UserTable.destroy(
+          {
+            where: {
+              email: email
+            }
+          }).then((returnValue) => {
+          if (returnValue === 0) {
+            reject(new Error('Nenhum elemento deletado'))
+          }
+          resolve('Deleted ' + returnValue + ' User(s) sucessfully')
+        }).catch((error) => {
+          reject(error)
+        })
+      })
+    }
+  }
+  return new UserController(tables)
+}
---
+++ routes/userRoute.js
@@ -0,0 +1,45 @@
+var express = require('express')
+var app = express()
+
+module.exports = function (userController) {
+
+    app.post('/', function (req, res) {
+        userController.createUser(req.body).then((User) => {
+          res.send(User)
+        }).catch((error) => {
+          res.send(error)
+        })
+      })
+    
+      app.get('/', function (req, res) {
+        var promise
+        if (req.query.email != null) {
+          promise = userController.readUser(req.query.email)
+        } else {
+          promise = userController.readAllUsers()
+        }
+        promise.then((User) => {
+          res.send(User)
+        }).catch((error) => {
+          res.send(error)
+        })
+      })
+    
+      app.put('/', function (req, res) {
+        userController.updateUser(req.query.email, req.body).then((returnObject) => {
+          res.send(returnObject)
+        }).catch((error) => {
+          res.send(error)
+        })
+      })
+    
+      app.delete('/', function (req, res) {
+        userController.deleteUser(req.query.email).then((returnValue) => {
+          res.send(returnValue)
+        }).catch((error) => {
+          res.send(error)
+        })
+      })
+    
+    return app;
+}
